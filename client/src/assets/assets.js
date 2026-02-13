99197f01e1e8a37251e28429af9179c1ed1ab08b
Asit Mandi <asitmandi86@gmail.com>
2026-02-14 00:21:56 +0530

    update trailer funtion

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -17,20 +17,20 @@ export const assets = {
 export const dummyTrailers = [
     {
         image: "https://img.youtube.com/vi/WpW36ldAqnM/maxresdefault.jpg",
-        videoUrl: "https://www.youtube.com/watch?v=Pez37wmUaQM&t=5837s"
+        videoUrl: "https://www.youtube.com/watch?v=WpW36ldAqnM"
     },
     {
         image: "https://img.youtube.com/vi/-sAOWhvheK8/maxresdefault.jpg",
-        videoUrl: 'https://www.youtube.com/watch?v=-sAOWhvheK8'
+        videoUrl: "https://www.youtube.com/watch?v=-sAOWhvheK8"
     },
     {
         image: "https://img.youtube.com/vi/1pHDWnXmK7Y/maxresdefault.jpg",
-        videoUrl: 'https://www.youtube.com/watch?v=1pHDWnXmK7Y'
+        videoUrl: "https://www.youtube.com/watch?v=1pHDWnXmK7Y"
     },
     {
         image: "https://img.youtube.com/vi/umiKiW4En9g/maxresdefault.jpg",
-        videoUrl: 'https://www.youtube.com/watch?v=umiKiW4En9g'
-    },
+        videoUrl: "https://www.youtube.com/watch?v=umiKiW4En9g"
+    }
 ]
 
 const dummyCastsData = [
--- client/src/componets/TrailersSection.jsx
+++ client/src/componets/TrailersSection.jsx
@@ -12,9 +12,9 @@ const TrailersSection = () => {
 
             <div className="relative mt-6">
                 <BlurCircle top="-100px" right="-100px"/>
-                <ReactPlayer url={currentTrailer.videoUrl} controls={false} className="mx-auto max-w-full" width="950px" height="540px"/>
+                <ReactPlayer url={currentTrailer.videoUrl} controls={false} className="mx-auto max-w-full" width="960px" height="540px"/>
             </div>
-
+        
             <div className="group grid grid-cols-4 gap-4 md:gap-8 mt-8 max-w-3xl mx-auto">
                 {dummyTrailers.map((trailer)=>(
                     <div key={trailer.image} className="relative group-hover:not-hover:opacity-50 hover:-translate-y-1 duration-300 transition max-md:h-60 cursor-pointer" onClick={()=>setCurrentTrailer(trailer)}>
